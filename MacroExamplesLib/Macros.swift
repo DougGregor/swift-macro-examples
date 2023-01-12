15edb81e9e9cae3673b3cf8d54beb38acd5659e7
Doug Gregor <dgregor@apple.com>
2023-01-11 16:25:12 -0800

    Update syntax for more recent snapshots

--- MacroExamplesLib/Macros.swift
+++ MacroExamplesLib/Macros.swift
@@ -1,15 +1,15 @@
 
 /// "Stringify" the provided value and produce a tuple that includes both the
 /// original value as well as the source code that generated it.
-public macro stringify<T>(_ value: T) -> (T, String) = MacroExamplesPlugin.StringifyMacro
+@expression public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "MacroExamplesPlugin", type: "StringifyMacro")
 
 /// Macro that produces a warning on "+" operators within the expression, and
 /// suggests changing them to "-".
-public macro addBlocker<T>(_ value: T) -> T = MacroExamplesPlugin.AddBlocker
+@expression public macro addBlocker<T>(_ value: T) -> T = #externalMacro(module: "MacroExamplesPlugin", type: "AddBlocker")
 
 /// Macro that produces a warning, as a replacement for the built-in
 /// #warning("...").
-public macro myWarning(_ message: String) = MacroExamplesPlugin.WarningMacro
+@expression public macro myWarning(_ message: String) = #externalMacro(module: "MacroExamplesPlugin", type: "WarningMacro")
 
 public enum FontWeight {
   case thin
@@ -24,5 +24,5 @@ public protocol ExpressibleByFontLiteral {
 }
 
 /// Font literal similar to, e.g., #colorLiteral.
-public macro fontLiteral<T>(name: String, size: Int, weight: FontWeight) -> T = MacroExamplesPlugin.FontLiteralMacro
+@expression public macro fontLiteral<T>(name: String, size: Int, weight: FontWeight) -> T = #externalMacro(module: "MacroExamplesPlugin", type: "FontLiteralMacro")
   where T: ExpressibleByFontLiteral

08d47a6f93804bb1e6f272414a93168ba5e26903
Doug Gregor <dgregor@apple.com>
2023-03-08 16:43:37 -0800

    Add a Package manifest so that SwiftPM can build the macro
    This package manifest uses the macro target features currently under
    discussion to integrate macros into the build. Just `swift build` with
    an appropriate toolchain (March 8th or newer).

---
+++ MacroExamplesPlugin/Plugin.swift
@@ -0,0 +1,26 @@
+#if canImport(SwiftCompilerPlugin)
+import SwiftCompilerPlugin
+import SwiftSyntaxMacros
+
+@main
+struct MyPlugin: CompilerPlugin {
+  let providingMacros: [Macro.Type] = [
+    StringifyMacro.self,
+    AddBlocker.self,
+    WarningMacro.self,
+    FontLiteralMacro.self,
+    WrapStoredPropertiesMacro.self,
+    DictionaryStorageMacro.self,
+    ObservableMacro.self,
+    ObservablePropertyMacro.self,
+    AddCompletionHandlerMacro.self,
+    AddAsyncMacro.self,
+    CaseDetectionMacro.self,
+    MetaEnumMacro.self,
+    CodableKey.self,
+    CustomCodable.self,
+    OptionSetMacro.self,
+    NewTypeMacro.self,
+  ]
+}
+#endif
